5d8da220b502450af5b65f18eab072b5cf0dc6d4
Adrian Vicente <>
2025-05-12 12:59:59 +0200

    Ej03 boletin

---
+++ UT08/src/boletin/Ej03.java
@@ -0,0 +1,210 @@
+package boletin;
+
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Ej03 extends JFrame implements ActionListener {
+
+	/*
+	 * componentes de la ventana
+	 */
+
+	private JLabel contacto, telefono;
+	private JTextField campocontacto, campotelefono;
+	private JButton nuevo, buscar, limpiar;
+	private JPanel plabels, pbotones, ptotal;
+	private static String FICHERO = "C:\\Users\\AdrianV\\Desktop\\MiAgenda2.txt";
+
+	public Ej03() {
+
+		setLayout(new FlowLayout(FlowLayout.CENTER, 50, 40));
+
+		ptotal = new JPanel();
+		ptotal.setLayout(new GridLayout(2, 0, 5, 20));
+		add(ptotal);
+
+		plabels = new JPanel();
+		plabels.setLayout(new GridLayout(2, 2, 10, 10));
+		ptotal.add(plabels);
+
+		pbotones = new JPanel();
+		pbotones.setLayout(new GridLayout(0, 3, 10, 5));
+		ptotal.add(pbotones);
+
+		contacto = new JLabel("Nombre del contacto: ");
+		plabels.add(contacto);
+
+		telefono = new JLabel("Telefono: ");
+		plabels.add(telefono);
+
+		campocontacto = new JTextField();
+		plabels.add(campocontacto);
+
+		campotelefono = new JTextField();
+		plabels.add(campotelefono);
+
+		nuevo = new JButton("Nuevo");
+		pbotones.add(nuevo);
+		nuevo.addActionListener(this);
+
+		buscar = new JButton("Buscar");
+		pbotones.add(buscar);
+		buscar.addActionListener(this);
+
+		limpiar = new JButton("Limpiar");
+		pbotones.add(limpiar);
+		limpiar.addActionListener(this);
+
+	}
+
+	public static void main(String[] args) {
+
+		Ej03 e = new Ej03();
+
+		e.setVisible(true);
+		e.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		e.setLocationRelativeTo(null);
+		e.setTitle("Agenda telefonica");
+		e.setSize(500, 250);
+
+	}
+
+	/**
+	 * Añade un nuevo contacto a la agenda
+	 * 
+	 * @param neme nombre del contacto a añadir
+	 * @param tlf  telefono del contacto a añadir
+	 */
+
+	private static void addContacto(String name, int tlf) {
+
+		FileWriter fw = null;
+		PrintWriter pw = null;
+		boolean encontrado = false;
+		File f = new File(FICHERO);
+		try {
+			fw = new FileWriter(FICHERO, true);
+			pw = new PrintWriter(fw);
+
+			Scanner s = new Scanner(f);
+			while (s.hasNextLine() && !encontrado) {
+				String linea = s.nextLine();
+				String nameagenda = linea.substring(0, linea.indexOf('>')).trim();
+				if (name.compareToIgnoreCase(nameagenda) == 0) {
+					encontrado = true;
+				}
+			}
+
+			pw.println(name + " > " + tlf);
+
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "El contenido ya se encuentra en la agenda", "Aviso",
+					JOptionPane.WARNING_MESSAGE);
+		} finally {
+			try {
+				if (fw != null)
+					fw.close();
+			} catch (Exception e2) {
+				System.out.println("Error en el cierre del archivo de la agenda");
+			}
+		}
+
+	}
+
+	/**
+	 * Busca un contacto en la agenda por su nombre
+	 * 
+	 * @param name - es el nombre del contacto a buscar
+	 * @return el telefono del contacto, si lo ha encontrado 0 si no lo ha
+	 *         encontrado
+	 */
+	private static int BuscarContacto(String name) {
+		// Buscamos el name en la agenda
+		File f = new File(FICHERO);
+		int res = 0;
+		boolean encontrado = false;
+
+		try {
+			// Lectura
+			Scanner s = new Scanner(f);
+			while (s.hasNextLine() && !encontrado) {
+				String linea = s.nextLine();
+				String nameagenda = linea.substring(0, linea.indexOf('>')).trim();
+				if (name.compareToIgnoreCase(nameagenda) == 0) {
+					encontrado = true;
+					String tlf = linea.substring(linea.indexOf('>') + 1).trim();
+					res = Integer.parseInt(tlf);
+				}
+			}
+		} catch (FileNotFoundException lupita) {
+			JOptionPane.showMessageDialog(null, "El contenido indicado no se encuentra en la agenda", "Buscar",
+					JOptionPane.INFORMATION_MESSAGE);
+		}
+
+		return res;
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == nuevo) {
+			if ((campocontacto.getText().isBlank()) || (campotelefono.getText().isEmpty())) {
+				JOptionPane.showMessageDialog(null, "Debes indicar el nombre y telefono del nuevo contacto", "Aviso",
+						JOptionPane.INFORMATION_MESSAGE);
+			} else {
+				String nombre = campocontacto.getText().trim();
+				if (BuscarContacto(nombre) != 0) {
+					JOptionPane.showMessageDialog(null, "El contacto ya se encuentra en la agenda", "Error",
+							JOptionPane.ERROR_MESSAGE);
+				} else {
+					int numero = Integer.parseInt(campotelefono.getText());
+					boolean encontrado = false;
+
+					addContacto(nombre, numero);
+					campocontacto.setText("");
+					campotelefono.setText("");
+					JOptionPane.showMessageDialog(null, "El nuevo contacto ha sido guardado en la agenda",
+							"Nuevo Contacto", JOptionPane.INFORMATION_MESSAGE);
+				}
+
+			}
+
+		}
+		if (e.getSource() == limpiar) {
+			campocontacto.setText("");
+			campotelefono.setText("");
+		}
+
+		if (e.getSource() == buscar) {
+			if (campocontacto.getText().isBlank()) {
+				JOptionPane.showMessageDialog(null, "Debes indicar el nombre del contacto a buscar", "Aviso",
+						JOptionPane.INFORMATION_MESSAGE);
+			} else {
+				String nombre = campocontacto.getText().trim();
+				int telefono = BuscarContacto(nombre);
+				if (telefono != 0) {
+					campotelefono.setText(String.valueOf(telefono));
+				} else {
+					JOptionPane.showConfirmDialog(null, "", "", JOptionPane.INFORMATION_MESSAGE);
+				}
+
+			}
+		}
+
+	}
+
+}
